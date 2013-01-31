e556dddc2bf84fafb46f792b4cc787dd5838354f
Dan Arrington <dan.arrington@gmail.com>
2013-01-30 21:05:07 -0800

    NHibernate and Autofac hooked up to app_start

--- mvcSkeleton/Controllers/HomeController.cs
+++ mvcSkeleton/Controllers/HomeController.cs
@@ -4,9 +4,11 @@ namespace mvcSkeleton.Controllers
 {
     public class HomeController : Controller
     {
-         public ActionResult Index()
-         {
-             return View();
-         }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
--- mvcSkeleton/Global.asax.cs
+++ mvcSkeleton/Global.asax.cs
@@ -1,10 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+using System.Data.SqlClient;
+using System.Reflection;
+using System.Web.Management;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
 
 namespace mvcSkeleton
 {
@@ -18,6 +24,52 @@ namespace mvcSkeleton
 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var nhibConfig = BuildNHibernateConfiguration();
+            var factory = CreateSessionFactory(nhibConfig);
+            Autofacify(nhibConfig, factory);
+        }
+
+        private void Autofacify(Configuration nhibConfig, ISessionFactory factory)
+        {
+
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsImplementedInterfaces();
+           // builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>));
+            builder.RegisterInstance(nhibConfig).As<Configuration>().SingleInstance();
+            builder.RegisterInstance(factory).As<ISessionFactory>().SingleInstance();
+            builder.Register(x => x.Resolve<ISessionFactory>().OpenSession()).As<ISession>().InstancePerLifetimeScope();
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        private Configuration BuildNHibernateConfiguration()
+        {
+            return Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2008.ConnectionString(c => c.FromConnectionStringWithKey("connectionString")))
+                .Mappings(m => m.FluentMappings.AddFromAssembly(Assembly.GetExecutingAssembly()))
+                .ExposeConfiguration(BuildSchema)
+                .BuildConfiguration();
+        }
+
+        private ISessionFactory CreateSessionFactory(Configuration config)
+        {
+            try
+            {
+                return config.BuildSessionFactory();
+
+            }
+            catch (SqlException e)
+            {
+                throw new Exception("Cannot open database.  You may need to create the db before nhibernate can build the schema", e);
+            }
+        }
+
+        private void BuildSchema(Configuration config)
+        {
+            new SchemaUpdate(config)
+                .Execute(false, true);
         }
     }
 }
